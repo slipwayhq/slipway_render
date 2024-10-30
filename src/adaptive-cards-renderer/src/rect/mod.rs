832e268d33e7265861378479491e1dc87efe373e
James Thurley <james@thurley.com>
2024-10-30 20:19:09 +0000

    Remove unused move_from_origin_into

--- src/adaptive-cards-renderer/src/rect/mod.rs
+++ src/adaptive-cards-renderer/src/rect/mod.rs
@@ -1,16 +1,11 @@
 use imageproc::rect::Rect;
 
 mod contains;
-mod move_from_origin_into;
 
 // A copy of the imageproc Region trait so we can add contains for u32.
 pub(super) trait SlipwayRegion<T> {
     /// Whether this region contains the given point.
     fn contains(&self, x: T, y: T) -> bool;
-}
-
-pub(super) trait MoveableFromOrigin {
-    fn move_from_origin_into(&self, outer_rect: Rect) -> Rect;
 }
 
 #[derive(serde::Serialize, serde::Deserialize, Debug, Clone, Copy, PartialEq, Eq)]

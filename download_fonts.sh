54a37c5aad4035024d6ff9a02e62956a663ee318
James Thurley <james@thurley.com>
2025-06-06 17:52:31 +0100

    Update download fonts script to correct URL

--- download_fonts.sh
+++ download_fonts.sh
@@ -2,7 +2,7 @@
 
 mkdir -p fonts
 
-ROBOTO_FONT_URL="https://github.com/google/fonts/raw/refs/heads/main/ofl/roboto/Roboto%5Bwdth,wght%5D.ttf"
-ROBOTO_MONO_FONT_URL="https://github.com/google/fonts/raw/refs/heads/main/apache/robotomono/RobotoMono%5Bwght%5D.ttf"
+ROBOTO_FONT_URL="https://github.com/google/fonts/raw/refs/heads/main/ofl/roboto/Roboto%5Bwdth%2Cwght%5D.ttf"
+ROBOTO_MONO_FONT_URL="https://github.com/google/fonts/raw/refs/heads/main/ofl/robotomono/RobotoMono%5Bwght%5D.ttf"
 ./download_font.sh "./fonts" "Roboto.ttf" "$ROBOTO_FONT_URL"
 ./download_font.sh "./fonts" "RobotoMono.ttf" "$ROBOTO_MONO_FONT_URL"
